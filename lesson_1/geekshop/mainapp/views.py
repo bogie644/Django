75e5910e4ddf5b2cebd8a3411318b9add65ae2ec
Bogie644 <bogie@list.ru>
2019-07-11 23:37:48 +0300

    lesson_2

--- lesson_1/geekshop/mainapp/views.py
+++ lesson_1/geekshop/mainapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', context= {'username': 'антон'})
 
 def catalog(request):
     return render(request, 'mainapp/catalog.html')
